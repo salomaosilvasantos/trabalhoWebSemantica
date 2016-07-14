5f3e53ca303e4c83fe6d6caa33bd7bc27d3da406
Eduardo <edurdneto@hotmail.com>
2016-07-14 15:05:41 -0300

    gerei links semanticos tcu-tse, tcu-cgu, cgu-tse

--- liks_semanticos/silk_singlemachine_2.7.1.sh
+++ liks_semanticos/silk_singlemachine_2.7.1.sh
@@ -1,2 +1,8 @@
-java -DconfigFile=config/links_tcu_cgu.xml -jar silk.jar
+java -DconfigFile=config/links_tcu_cgu_140716.xml -jar silk.jar
+sleep 2
+
+java -DconfigFile=config/links_tcu_tse_140716.xml -jar silk.jar
+sleep 2
+
+java -DconfigFile=config/links_cgu_tse_140716.xml -jar silk.jar
 sleep 2
