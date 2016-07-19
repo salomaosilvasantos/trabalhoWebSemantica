358150355be20415e70a42fcb762c4cc6649ca2f
jonaslimads <jonaslimads@gmail.com>
2016-07-19 00:23:20 -0300

    temProvenance e temRestritor adicionado a todas as triplas de LDIF. Alem disso bugs foram corrigidos.

--- liks_semanticos/silk_singlemachine_2.7.1.sh
+++
@@ -1,8 +0,0 @@
-java -DconfigFile=config/links_tcu_cgu_140716.xml -jar silk.jar
-sleep 2
-
-java -DconfigFile=config/links_tcu_tse_140716.xml -jar silk.jar
-sleep 2
-
-java -DconfigFile=config/links_cgu_tse_140716.xml -jar silk.jar
-sleep 2
---
+++ links_semanticos/silk_singlemachine_2.7.1.bat
@@ -0,0 +1,2 @@
+java -DconfigFile=config/links_tcu_cgu.xml -jar silk.jar
+sleep 2
